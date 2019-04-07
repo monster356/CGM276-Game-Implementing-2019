26148c273cc358ca31f0e0cedc9cffcf42722c03
monster356 <ruksasut@hotmail.com>
2019-04-07 12:10:02 +0700

    Update server.js

--- 04-multiplayer_game_1/server/server.js
+++ 04-multiplayer_game_1/server/server.js
@@ -2,7 +2,26 @@ var io = require('socket.io')(process.env.PORT || 3000);
 
 console.log("serverStarted");
 
-io.on("connection",function(socket){
+var playarcount = 0;
+
+io.on("connection", function (socket) {
 
     console.log("clientConnected");
+
+    socket.broadcast.emit('spawn');
+    playarcount++;
+
+    for (i = 0; i < playarcount; i++) {
+        socket.emit("spawn");
+        console.log("spawn existing player");
+    }
+
+    socket.on("move", function (data) {
+        console.log("move");
+    })
+
+    socket.on("disconnect", function () {
+        console.log("client disconnect");
+        playarcount--;
+    })
 });
