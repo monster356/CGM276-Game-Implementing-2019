04236b4695d0f1446f9b9061243ce05ece80639b
monster356 <ruksasut@hotmail.com>
2019-04-07 12:16:22 +0700

    unity source

---
+++ 04-multiplayer_game_1/client/gameM1/Assets/Scripts/Network.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using SocketIO;
+public class Network : MonoBehaviour
+{
+    static SocketIOComponent socket;
+    public GameObject playerPrefab;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+
+        socket.On("spawn", OnSpawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnConnected(SocketIOEvent e)
+    {
+        print("connected");
+
+        socket.Emit("move");
+
+    }
+    void OnSpawn(SocketIOEvent e)
+    {
+        // Vector3 position = new Vector3(Random.Range(-5.0f, 5.0f), 1, Random.Range(-5.0f, 5.0f));
+        print("spawned");
+
+        Instantiate(playerPrefab);
+
+        //socket.Emit("move");
+
+    }
+}
